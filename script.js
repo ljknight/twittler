d1b1b91d4b239cff2608f287420f8251bbda5781
Laura Knight <ljknight@gmail.com>
2015-07-20 13:40:29 -0700

    Create script.js file, load one tweet at a time with button

---
+++ script.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+  var $body = $('body');
+  var $tweetStream = $('.tweets');
+  var $button = $('button');
+  
+  var showNewTweets = function() {
+    // var index = streams.home.length - 1;
+    // while(index >= 0){
+    // var tweet = streams.home[index];
+      
+      // loads one tweet at a times
+      var tweet = streams.home.pop();
+      
+      var $tweetContainer = $('<div></div>', {
+        'class' : 'tweetContainer'
+      });
+
+      // shows relative time 
+      var tweetDate = moment().startOf('hour').fromNow();
+      
+      // add user URL
+      var userURL = allUserURLS[tweet.user];
+      var userProfileLink = $('<a />', {
+        href: userURL,
+        target: "blank",
+        text: '@' + tweet.user
+      });
+
+      $tweetContainer.html('<a class="user">'+'@' + tweet.user +'</a>' + '<div class="date">'+tweetDate+'</div>' + '<div class="tweet">'+tweet.message+'</div>');
+
+      $tweetContainer.prependTo($tweetStream);
+      
+      //index -= 1;  
+
+    }
+
+    var tweetCount = $('.tweetContainer').length;
+    console.log(tweetCount, streams.home.length) 
+  
+  $button.click(function() {
+    //$('span').text(streams.home.length-tweetCount);
+    showNewTweets();
+  });
+   
+});
