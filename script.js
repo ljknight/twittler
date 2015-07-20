4934ebe4d2a79c26ec207bd8064dbb6bc547d808
Laura Knight <ljknight@gmail.com>
2015-07-20 13:45:14 -0700

    Loads 11 tweets on refresh, add next tweet with button

--- script.js
+++ script.js
@@ -3,10 +3,34 @@ $(document).ready(function(){
   var $tweetStream = $('.tweets');
   var $button = $('button');
   
+  var index = streams.home.length - 1;
+    while(index >= 0){
+    var tweet = streams.home[index];
+
+    var $tweetContainer = $('<div></div>', {
+        'class' : 'tweetContainer'
+      });
+
+      // shows relative time 
+      var tweetDate = moment().startOf('hour').fromNow();
+      
+      // add user URL
+      var userURL = allUserURLS[tweet.user];
+      var userProfileLink = $('<a />', {
+        href: userURL,
+        target: "blank",
+        text: '@' + tweet.user
+      });
+
+      $tweetContainer.html('<a class="user">'+'@' + tweet.user +'</a>' + '<div class="date">'+tweetDate+'</div>' + '<div class="tweet">'+tweet.message+'</div>');
+
+      $tweetContainer.prependTo($tweetStream);
+      
+    index--;
+  };
+
   var showNewTweets = function() {
-    // var index = streams.home.length - 1;
-    // while(index >= 0){
-    // var tweet = streams.home[index];
+    
       
       // loads one tweet at a times
       var tweet = streams.home.pop();
@@ -28,9 +52,7 @@ $(document).ready(function(){
 
       $tweetContainer.html('<a class="user">'+'@' + tweet.user +'</a>' + '<div class="date">'+tweetDate+'</div>' + '<div class="tweet">'+tweet.message+'</div>');
 
-      $tweetContainer.prependTo($tweetStream);
-      
-      //index -= 1;  
+      $tweetContainer.prependTo($tweetStream); 
 
     }
 
