afdbb31397bbe4156863a498d7386b2a008f1425
Laura Knight <ljknight@gmail.com>
2015-07-20 21:41:38 -0700

    Add refresh button & refresh on logo click

--- script.js
+++ script.js
@@ -1,35 +1,53 @@
 $(document).ready(function(){
   var $body = $('body');
+  var $twittler = $('h1');
   var $tweetStream = $('.tweet-stream');
   var $showNext = $('.show-next');
   var $showAll = $('.show-all');
+  var $refresh = $('.refresh');
+  var formatTweet;
+  var $userLink;
   
   // grabs next tweet and formats
   var grabTweet = function() {
     var tweet = streams.home.pop();
-    var $tweetContainer = $('<div></div>', {
-        'class' : 'tweet-container'
-      });
+    
+    formatTweet = function() {
+      var $tweetContainer = $('<div></div>', {
+          'class' : 'tweet-container'
+        });
 
-    // shows relative time 
-    var tweetDate = moment().startOf('hour').fromNow();
-      
-    // assign avatar
-    var avatarURL = avatars[tweet.user];
+      // shows relative time 
+      var tweetDate = moment().startOf('hour').fromNow();
+        
+      // assign avatar to tweet.user
+      var avatarURL = avatars[tweet.user];
 
-    $tweetContainer.html('<img class="avatar" src="'+avatarURL+'"</>'+ '<a class="user">'+'@' + tweet.user +'</a>' + '<div class="date">'+tweetDate+'</div>' + '<div class="tweet-message">'+tweet.message+'</div>');
+      // gives tweet-container a class of its username
+      $tweetContainer.addClass(tweet.user);
 
-    $tweetContainer.prependTo($tweetStream);
-  }
+      $tweetContainer.html('<a class="user-link"><img class="avatar" src="'+avatarURL+'"</>'+ '<div class="user">'+'@' + tweet.user +'</div></a>' + '<div class="date">'+tweetDate+'</div>' + '<div class="tweet-message">'+tweet.message+'</div>');
+
+      $tweetContainer.prependTo($tweetStream);
+  
+    }
+
+    formatTweet();
+
+  };
 
   // loads 11 tweets on refresh
-  var index = streams.home.length - 1;
-    while(index >= 0){
-    
-    grabTweet();
+  var refresh = function() {
+    var index = streams.home.length - 1;
+      while(index >= 0){
       
-    index--;
+      grabTweet();
+        
+      index--;
+    };
   };
+
+  refresh();
 
   // loads one tweet at a time
   $showNext.click(function() {
@@ -38,9 +56,26 @@ $(document).ready(function(){
 
   // loads all tweets
   $showAll.click(function() {
-    while(streams.home.length >=0)
-
-    grabTweet();    
+    while(streams.home.length >=0) {
+      grabTweet(); 
+    }
   });
-   
+
+  // refreshes feed
+  $refresh.click(function() {
+    $tweetStream.html('');
+    refresh();
+  });
+
+  // hard refresh for logo click
+  $twittler.click(function() {
+    location.reload();
+  });
+
+  // $userLink.click(function() {
+  //   var clickUser = $(this).closest('.tweet-container').;
+  //   console.log(clickUser);
+  //   $('.tweet-container').not('.' + clickUser).fadeOut();
+    
+  // });
 });
