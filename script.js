2fa7e40cde388abf729b03e0276e21ecec96423c
Laura Knight <ljknight@gmail.com>
2015-07-21 12:56:08 -0700

    Fixes time stamp to display correct relative time

--- script.js
+++ script.js
@@ -1,88 +1,69 @@
 var $body = $('body');
-  var $twittler = $('h1');
   var $tweetStream = $('.tweet-stream');
   var $showNext = $('.show-next');
   var $showAll = $('.show-all');
   var $refresh = $('.refresh');
-  var $showingUser = $('.showing-user');
+  var $showingUserText = $('.showing-user-text');
+  var $tweetContainer;
   var formatTweet;
-  var $userLink;
-  var userSorted = false;
+  var userClass;
 
 $(document).ready(function(){
-  
-  
   // grabs next tweet and formats
   var grabTweet = function() {
-    var tweet = streams.home.pop();
+    var tweet = streams.home.shift();
     
     formatTweet = function() {
-      var $tweetContainer = $('<div></div>', {
+      $tweetContainer = $('<div></div>', {
           'class' : 'tweet-container'
         });
 
-      // shows relative time 
-      var tweetDate = moment().startOf('hour').fromNow();
-        
-      // assign avatar to tweet.user
-      var avatarURL = avatars[tweet.user];
+      var date = new Date();
+      var tweetDate = date.toISOString();
+
+      $tweetContainer.html('<a class="user-link"><img class="avatar" src="'+avatars[tweet.user]+'"</>'+ '<div class="user">'+'@' + tweet.user +'</div></a>' + '<div class="date" title="'+tweetDate+'"></div>' + '<div class="tweet-message">'+tweet.message+'</div>');
 
       // gives tweet-container a class of its username
       $tweetContainer.addClass(tweet.user);
 
-      $tweetContainer.html('<a class="user-link '+tweet.user+'"><img class="avatar" src="'+avatarURL+'"</>'+ '<div class="user">'+'@' + tweet.user +'</div></a>' + '<div class="date">'+tweetDate+'</div>' + '<div class="tweet-message">'+tweet.message+'</div>');
-
+      // add tweet to stream
       $tweetContainer.fadeIn().prependTo($tweetStream);
   
     }
-
+    
     formatTweet();
-
+    
+    // creates relative time stamp
+    $("div.date").timeago();
   };
 
-  // loads 11 tweets on refresh
-  var refresh = function() {
-    var index = streams.home.length - 1;
-      while(index >= 0){
-      
-      grabTweet();
-        
-      index--;
-    };
+  // loads 10 tweets on refresh
+  while(streams.home.length-1 > 0){
+    grabTweet();
   };
-
-  refresh();
 
   // loads one tweet at a time
   $showNext.click(function() {
-      grabTweet();
+    grabTweet();
   });
 
   // loads all tweets
   $showAll.click(function() {
-    while(streams.home.length >=0) {
-      grabTweet(); 
+    while(streams.home.length > 0) {
+      grabTweet();
     }
   });
 
-  // refreshes feed
+  // refreshes feed with refresh button and logo click
   $refresh.click(function() {
     location.reload();
   });
 
-  // refresh on logo click
-  $twittler.click(function() {
-    location.reload();
-  });
-
-  $('.tweet-container').click(function() {
-    var userClass = $(this).closest('.tweet-container').attr('class').split(' ')[1];
-    console.log(userClass);
-    //$('div').find(userClass).show();
-    // var clickUser = $(this).closest('.tweet-container').;
-    // console.log(clickUser);
-    $showingUser.prependTo($tweetStream).text('Showing tweets by @' + userClass);
-    $('.tweet-container:not(.'+userClass+')').fadeOut();
-    userSorted = true;
+  $('a').click(function() {
+    userClass = $(this).closest('.tweet-container').attr('class').split(' ')[1];
+    $showingUserText.prependTo($tweetStream).text('Showing tweets by @' + userClass);
+    $('.tweet-container:not(.'+userClass+')').fadeOut(500, function() { 
+      $(this).remove(); 
+    })
   });
 });
