95a3a00b083a5805861796fcc6494cfd82d22d1d
Laura Knight <ljknight@gmail.com>
2015-07-22 12:40:50 -0700

    Cleans up styles, combines grabRandomTweet & formatRandomtweet, adds better comments

--- script.js
+++ script.js
@@ -4,9 +4,7 @@ var $composeForm = $('.compose-form');
 var $submitTweet = $('.submit-tweet');
 $composeForm.hide();
 $submitTweet.hide();
-$countdown = $('.countdown');
 var $composeTweet = $('.compose-tweet');
-var tweetContent;
 var $showNext = $('.show-next');
 var $showAll = $('.show-all');
 var $refresh = $('.refresh');
@@ -16,35 +14,35 @@ var userClass;
 
 $(document).ready(function(){
   var tweet;
-  // grabs random next tweet and formats
+
+  // grabs next random tweet
   var grabRandomTweet = function() {
     tweet = streams.home.shift();
     
+    // formats random tweet
+    var formatRandomTweet = function() {
+      $tweetContainer = $('<div></div>', {
+          'class' : 'tweet-container'
+        });
+
+      var date = new Date();
+      var tweetDate = date.toISOString();
+
+      $tweetContainer.html('<a class="user-link"><img class="avatar" src="'+avatars[tweet.user]+'" title="Show all tweets by @'+tweet.user+'"</>'+ '<div class="user">'+'@' + tweet.user +'</div></a>' + '<div class="date" title="'+tweetDate+'"></div>' + '<div class="tweet-message">'+tweet.message+'</div>');
+
+      // gives tweet-container a class of its username
+      $tweetContainer.addClass(tweet.user);
+
+      // add tweet to stream
+      $tweetContainer.fadeIn().prependTo($tweetStream);
+
+    };
     formatRandomTweet();
-    
     // creates relative time stamp
     $("div.date").timeago();
   };
 
-  // formats random tweet
-  var formatRandomTweet = function() {
-    $tweetContainer = $('<div></div>', {
-        'class' : 'tweet-container'
-      });
-
-    var date = new Date();
-    var tweetDate = date.toISOString();
-
-    $tweetContainer.html('<a class="user-link"><img class="avatar" src="'+avatars[tweet.user]+'" title="Show all tweets by @'+tweet.user+'"</>'+ '<div class="user">'+'@' + tweet.user +'</div></a>' + '<div class="date" title="'+tweetDate+'"></div>' + '<div class="tweet-message">'+tweet.message+'</div>');
-
-    // gives tweet-container a class of its username
-    $tweetContainer.addClass(tweet.user);
-
-    // add tweet to stream
-    $tweetContainer.fadeIn().prependTo($tweetStream);
-
-  };
-
+  // formats tweet for anonymous user input
   var formatAnonymousTweet = function() {
     $tweetContainer = $('<div></div>', {
         'class' : 'tweet-container'
@@ -65,7 +63,7 @@ $(document).ready(function(){
   // loads 10 tweets on refresh
   while(streams.home.length-1 > 0){
     grabRandomTweet();
-  };
+  }
 
   // compose button effects
   var showCompose = function() {
@@ -73,14 +71,14 @@ $(document).ready(function(){
     $submitTweet.fadeIn(500);
     $composeTweet.removeClass('hover');
     $(this).one('click', hideCompose);
-  }
+  };
 
   var hideCompose = function() {
     $composeForm.fadeOut(500);
     $submitTweet.fadeOut(500);
     $composeTweet.addClass('hover');
     $(this).one('click', showCompose);
-  }
+  };
 
   $composeTweet.one('click', showCompose);
 
@@ -93,7 +91,7 @@ $(document).ready(function(){
     $submitTweet.fadeOut(500);
     $composeTweet.addClass('hover');
     $composeForm.val('');
-  })
+  });
 
   $composeForm.keypress(function(e){
     if (e.which === 13) {
@@ -104,14 +102,14 @@ $(document).ready(function(){
       $composeTweet.addClass('hover');
       $composeForm.val('');
     }
-  })
+  });
 
-  // loads one tweet at a time
+  // loads one random tweet at a time
   $showNext.click(function() {
     grabRandomTweet();
   });
 
-  // loads all tweets
+  // loads all random tweets
   $showAll.click(function() {
     while(streams.home.length > 0) {
       grabRandomTweet();
@@ -123,12 +121,12 @@ $(document).ready(function(){
     location.reload();
   });
 
-  // works on closest a 
+  // works on closest <a> 
   $('body').on('click', 'a', function() {
     userClass = $(this).closest('.tweet-container').attr('class').split(' ')[1]; 
     $showingUserText.prependTo($tweetStream).text('Showing tweets by @' + userClass);
     $('.tweet-container:not(.'+userClass+')').fadeOut(500, function() { 
       $(this).remove(); 
-    })
+    });
   });
 });
